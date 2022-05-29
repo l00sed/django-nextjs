eb3e2121b52b8aca94540ace1a067d5b8a259cae
l00sed <danielwtompkins@gmail.com>
2022-05-29 15:23:22 -0400

    updated styles

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: [
+      'localhost',
+      '127.0.0.1',
+      '0.0.0.0',
       '192.168.0.104',
       '192.168.0.104:8000',
       '192.168.0.104:3000',
