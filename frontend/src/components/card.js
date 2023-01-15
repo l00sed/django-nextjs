13ee2c59452aec2af2289b261d2556e2cb8ab306
l00sed <danielwtompkins@gmail.com>
2023-01-15 03:14:58 -0500

    Updated next, not using app dir. Updated some of the legacy Link components.

--- frontend/src/components/card.js
+++ frontend/src/components/card.js
@@ -8,29 +8,27 @@ import dateformat from 'dateformat'
 export default function Card({ element }) {
   return (
     <Link key={ element.id } href={ '/' + element.slug }>
-      <a>
-        <div className={ styles.card }>
-          <div className={ styles.card__thumbnail_wrapper }>
-            <Image
-              alt={ element.image_alt }
-              src={ element.featured_image }
-              className={ styles.card__thumbnail }
-              objectFit={'cover'}
-              layout={'fill'}
-            />
-          </div>
-          <div className={ styles.card__head }>
-            <h2 className={ styles.card__title }>{ element.title }</h2>
-            <div className={ styles.card__meta }>
-              <span className={ styles.card__date }>{ dateformat( new Date(element.updated_at), "h:MMTT | mmmm, dS yyyy") }</span>
-              <span className={ styles.card__author }>{ element.author }</span>
-            </div>
-          </div>
-          <div className={ styles.card__body }>
-            <div className={ styles.card__description }>{ Parse(element.description) }</div>
+      <div className={ styles.card }>
+        <div className={ styles.card__thumbnail_wrapper }>
+          <Image
+            alt={ element.image_alt }
+            src={ element.featured_image }
+            className={ styles.card__thumbnail }
+            objectFit={'cover'}
+            layout={'fill'}
+          />
+        </div>
+        <div className={ styles.card__head }>
+          <h2 className={ styles.card__title }>{ element.title }</h2>
+          <div className={ styles.card__meta }>
+            <span className={ styles.card__date }>{ dateformat( new Date(element.updated_at), "h:MMTT | mmmm, dS yyyy") }</span>
+            <span className={ styles.card__author }>{ element.author }</span>
           </div>
         </div>
-      </a>
+        <div className={ styles.card__body }>
+          <div className={ styles.card__description }>{ Parse(element.description) }</div>
+        </div>
+      </div>
     </Link>
   )
 }
