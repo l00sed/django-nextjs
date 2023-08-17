134c2be27e220fbfeeec1446bf9db62d416ec139
l00sed <danielwtompkins@gmail.com>
2023-08-17 03:28:38 -0400

    Removed unnecessary polling webpack config setting. Updated parseTitle setup for long titles (with non-english words) to hyphenate and wrap.

--- app/frontend/src/utils/parser.js
+++ app/frontend/src/utils/parser.js
@@ -7,3 +7,15 @@ export default function Parse(input) {      // Code syntax highlighting
     return
   }
 }
+
+export const parseTitle = (element) => {
+  let long_titles = {
+    "abstract": <>Abstract&shy;ions II</>,
+    "microfactory-2": <>Micro&shy;fact&shy;ory 02</>
+  }
+  if (Object.keys(long_titles).includes(element.slug)) {
+    return long_titles[element.slug]
+  } else {
+    return element.title
+  }
+}
